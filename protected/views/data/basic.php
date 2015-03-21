12f551cc0589dabf9efd40614107e3ed389becd7
taohs <50584211@qq.com>
2015-03-21 17:24:05 +0800

    update 基本挂账单 和 拓展挂账单 排序

--- protected/views/data/basic.php
+++ protected/views/data/basic.php
@@ -1,5 +1,36 @@
 <div class="right">
     <div class="body" style="margin-top: 0px;">
+
+        <?php $this->widget('zii.widgets.grid.CGridView',array(
+            'dataProvider'=>$dataProvider,
+            'template'=>"{items}<div class='page'>{pager}{summary}</div>",
+            'itemsCssClass'=>'list item',
+            'pagerCssClass'=>'abdadfasdf',
+            'columns'=>array(
+
+                array('name'=>'编号','value'=>'$data->code'),
+                array('name'=>'品名','value'=>'$data->name'),
+                array('name'=>'规格型号','value'=>'$data->format'),
+                array('name'=>'计量单位','value'=>'$data->unit'),
+                array('name'=>'本期收入数量','value'=>'$data->current_number'),
+                array('name'=>'本期挂帐数量','value'=>'$data->current_unpaid_debts'),
+                array('name'=>'单价','value'=>'$data->price'),
+                array('name'=>'去税金额', 'value'=>'$data->after_tax'),
+                array('name'=>'税额', 'value'=>'$data->tax'),
+                array('name'=>'挂帐金额', 'value'=>'$data->unpaid_debts_amount'),
+
+            ),
+            'pager'=>array(
+                // 'pagerCssClass'=>'page',
+                'nextPageLabel'=>'下一页',
+                'prevPageLabel'=>'上一页',
+                'header'=>'',
+                'cssFile'=>false,
+                'internalPageCssClass'=>false,
+                'maxButtonCount'=>7
+            )
+        ));?>
+
         <div style="margin-top:15px">
             <table class="list">
                 <thead>
@@ -37,35 +68,6 @@
             </table>
 
         </div>
-        <?php $this->widget('zii.widgets.grid.CGridView',array(
-            'dataProvider'=>$dataProvider,
-            'template'=>"{items}<div class='page'>{pager}{summary}</div>",
-            'itemsCssClass'=>'list item',
-            'pagerCssClass'=>'abdadfasdf',
-            'columns'=>array(
-
-                array('name'=>'编号','value'=>'$data->code'),
-                array('name'=>'品名','value'=>'$data->name'),
-                array('name'=>'规格型号','value'=>'$data->format'),
-                array('name'=>'计量单位','value'=>'$data->unit'),
-                array('name'=>'本期收入数量','value'=>'$data->current_number'),
-                array('name'=>'本期挂帐数量','value'=>'$data->current_unpaid_debts'),
-                array('name'=>'单价','value'=>'$data->price'),
-                array('name'=>'去税金额', 'value'=>'$data->after_tax'),
-                array('name'=>'税额', 'value'=>'$data->tax'),
-                array('name'=>'挂帐金额', 'value'=>'$data->unpaid_debts_amount'),
-
-            ),
-            'pager'=>array(
-                // 'pagerCssClass'=>'page',
-                'nextPageLabel'=>'下一页',
-                'prevPageLabel'=>'上一页',
-                'header'=>'',
-                'cssFile'=>false,
-                'internalPageCssClass'=>false,
-                'maxButtonCount'=>7
-            )
-        ));?>
     </div>
 </div>
 <script type="text/javascript">
--- protected/views/data/extension.php
+++ protected/views/data/extension.php
@@ -1,5 +1,35 @@
 <div class="right">
     <div class="body" style="margin-top: 0px;">
+
+        <?php $this->widget( 'zii.widgets.grid.CGridView', array(
+            'dataProvider' => $dataProvider,
+            'template' => "{items}<div class='page'>{pager}{summary}</div>",
+            'itemsCssClass' => 'list item',
+            'pagerCssClass' => 'abdadfasdf',
+            'columns' => array(
+                array( 'name' => '编号', 'value' => '$data->code' ),
+                array( 'name' => '品名', 'value' => '$data->name' ),
+                array( 'name' => '规格型号', 'value' => '$data->format' ),
+                array( 'name' => '计量单位', 'value' => '$data->unit' ),
+                array( 'name' => '上期结存数量', 'value' => '$data->last_number' ),
+                array( 'name' => '本期收入数量', 'value' => '$data->current_number' ),
+                array( 'name' => '本期挂帐数量', 'value' => '$data->current_unpaid_debts' ),
+                array( 'name' => '单价', 'value' => '$data->price' ),
+                array( 'name' => '挂帐金额', 'value' => '$data->unpaid_debts_amount' ),
+                array( 'name' => '本期未挂帐数量', 'value' => '$data->no_unpaid_debts' ),
+                array( 'name' => '期末结存金额', 'value' => '$data->close_amount' ),
+            ),
+            'pager' => array(
+                // 'pagerCssClass'=>'page',
+                'nextPageLabel' => '下一页',
+                'prevPageLabel' => '上一页',
+                'header' => '',
+                'cssFile' => false,
+                'internalPageCssClass' => false,
+                'maxButtonCount' => 7
+            )
+        ) ); ?>
+
         <div style="margin-top:15px">
             <table class="list">
                 <thead>
@@ -10,20 +40,21 @@
                     <th>本期未挂帐数</th>
                     <th>期末结存金额</th>
                     <th>期初应付账款余额</th>
-                    <th>质保金</th>
+                    <th>本期应付账款余额</th>
+
                     <th>扣点</th>
                 </tr>
                 </thead>
                 <tboty>
                     <tr>
-                        <td><?php echo $total->current_number;?></td>
-                        <td><?php echo $total->current_unpaid_debts;?></td>
-                        <td><?php echo $total->unpaid_debts_amount;?></td>
-                        <td><?php echo $total->no_unpaid_debts;?></td>
-                        <td><?php echo $total->close_amount;?></td>
-                        <td><?php echo $total->open_balance_payable;?></td>
-                        <td><?php echo $total->retention_money;?></td>
-                        <td><?php echo $total->deduction;?></td>
+                        <td><?php echo $total->current_number; ?></td>
+                        <td><?php echo $total->current_unpaid_debts; ?></td>
+                        <td><?php echo $total->unpaid_debts_amount; ?></td>
+                        <td><?php echo $total->no_unpaid_debts; ?></td>
+                        <td><?php echo $total->close_amount; ?></td>
+                        <td><?php echo $total->open_balance_payable; ?></td>
+                        <td><?php echo $total->current_balance_payable; ?></td>
+                        <td><?php echo $total->deduction; ?></td>
                     </tr>
                 </tboty>
             </table>
@@ -38,67 +69,34 @@
                     <th>本期网付金额</th>
                     <th>本期卡付金额</th>
                     <th>其中承兑额</th>
-                    <th>本期应付账款余额</th>
+                    <th>质保金</th>
+
                 </tr>
                 </thead>
                 <tboty>
                     <tr>
-                        <!--<td>--><?php //echo $total->fine;?><!--</td>-->
-                        <!--<td>--><?php //echo $total->transportation_fee;?><!--</td>-->
-                        <!--<td>--><?php //echo $total->check_fee;?><!--</td>-->
-                        <!--<td>--><?php //echo number_format($total->other_fee,2,".","");?><!--</td>-->
-                        <!--<td>--><?php //echo number_format($total->current_net_amount_paid,2,".","");?><!--</td>-->
-                        <!--<td>--><?php //echo number_format($total->current_card_amount_paid,2,".","");?><!--</td>-->
-                        <!--<td>--><?php //echo number_format($total->acceptances_amount,2,".","");?><!--</td>-->
-                        <!--<td>--><?php //echo number_format($total->current_balance_payable,2,".","");?><!--</td>-->
-
-                        <td><?php echo $total->fine;?></td>
-                        <td><?php echo number_format($total->transportation_fee,2,".","");?></td>
-                        <td><?php echo number_format($total->check_fee,2,".","");?></td>
-                        <td><?php echo number_format($total->other_fee,2,".","");?></td>
-                        <td><?php echo number_format($total->current_net_amount_paid,2,".","");?></td>
-                        <td><?php echo number_format($total->current_card_amount_paid,2,".","");?></td>
-                        <td><?php echo number_format($total->acceptances_amount,2,".","");?></td>
-                        <td><?php echo number_format($total->current_balance_payable,2,".","");?></td>
+                        <td><?php echo $total->fine; ?></td>
+                        <td><?php echo number_format( $total->transportation_fee, 2, ".", "" ); ?></td>
+                        <td><?php echo number_format( $total->check_fee, 2, ".", "" ); ?></td>
+                        <td><?php echo number_format( $total->other_fee, 2, ".", "" ); ?></td>
+                        <td><?php echo number_format( $total->current_net_amount_paid, 2, ".", "" ); ?></td>
+                        <td><?php echo number_format( $total->current_card_amount_paid, 2, ".", "" ); ?></td>
+                        <td><?php echo number_format( $total->acceptances_amount, 2, ".", "" ); ?></td>
+                        <td><?php echo number_format( $total->retention_money, 2, ".", "" ); ?></td>
                     </tr>
                 </tboty>
             </table>
 
         </div>
-        <?php $this->widget('zii.widgets.grid.CGridView',array(
-            'dataProvider'=>$dataProvider,
-            'template'=>"{items}<div class='page'>{pager}{summary}</div>",
-            'itemsCssClass'=>'list item',
-            'pagerCssClass'=>'abdadfasdf',
-            'columns'=>array(
-                array('name'=>'编号','value'=>'$data->code'),
-                array('name'=>'品名','value'=>'$data->name'),
-                array('name'=>'规格型号','value'=>'$data->format'),
-                array('name'=>'计量单位','value'=>'$data->unit'),
-                array('name'=>'上期结存数量','value'=>'$data->last_number'),
-                array('name'=>'本期收入数量','value'=>'$data->current_number'),
-                array('name'=>'本期挂帐数量','value'=>'$data->current_unpaid_debts'),
-                array('name'=>'单价', 'value'=>'$data->price'),
-                array('name'=>'挂帐金额', 'value'=>'$data->unpaid_debts_amount'),
-                array('name'=>'本期未挂帐数量', 'value'=>'$data->no_unpaid_debts'),
-                array('name'=>'期末结存金额', 'value'=>'$data->close_amount'),
-            ),
-            'pager'=>array(
-                // 'pagerCssClass'=>'page',
-                'nextPageLabel'=>'下一页',
-                'prevPageLabel'=>'上一页',
-                'header'=>'',
-                'cssFile'=>false,
-                'internalPageCssClass'=>false,
-                'maxButtonCount'=>7
-            )
-        ));?>
     </div>
 </div>
 <script type="text/javascript">
-    $("#search").click(function(){
-        var url=window.location.href;
-        url=url + '&year='+$("#year").val();
-        window.open(url.'_self');
+    $("#search").click(function () {
+        var url = window.location.href;
+        url = url + '&year=' + $("#year").val();
+        window.open(url.
+        '_self'
+        )
+        ;
     });
 </script>
