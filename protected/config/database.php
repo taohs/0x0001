236bc718b6141310164d46e6eb2bd187ee037bee
taohs <50584211@qq.com>
2015-03-24 09:05:57 +0800

    update 公司搜索用单位编码和名称搜索 update 删除公司信息 选择公司编码 update 用户更新数据时选择公司为公司编码 update 基础表和拓展表新加备注字段，拓展表统计项目修改

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,10 +5,20 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost;dbname=20141204',
+	'connectionString' => 'mysql:host=localhost;dbname=201412061224',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '1q2w3e',
 	'charset' => 'utf8',
+
+
+/**
+ *  服务器配置
+ */
+//    'connectionString' => 'mysql:host=localhost;dbname=201412061224',
+//    'emulatePrepare' => true,
+//    'username' => 'root',
+//    'password' => '1q2w3e4r',
+//    'charset' => 'utf8',
 
 );
